3a6600fba3592d9e3d384bd4d663b978d4adb5e3
Juan C. Rodriguez <jcrodriguezw@gmail.com>
2017-11-23 15:13:09 -0500

    haciendo la barra responsive

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -6,29 +6,43 @@ import NavItem from 'react-bootstrap/lib/NavItem'
 import MenuItem from 'react-bootstrap/lib/MenuItem'
 import NavDropdown from 'react-bootstrap/lib/NavDropdown'
 
+
 class NavBar extends React.Component {
     render() {
         return(
-            <Navbar>
+            <Navbar collapseOnSelect>
 
                 <Navbar.Header>
                     <Navbar.Brand>
-                        <a href="#"> ReCoTuTo </a>
+                        <a href="#"> Reyna MakeUp </a>
                     </Navbar.Brand>
+                    <Navbar.Toggle />
                 </Navbar.Header>
 
                 <Navbar.Collapse>
                     <Nav>
-                        <NavItem eventKey={1} href="#">Posts</NavItem>
-                        <NavItem eventKey={1} href="#">Shouts</NavItem>
-                        <NavItem eventKey={1} href="#">Comunidades</NavItem>
-                        <NavItem eventKey={1} href="#">Juegos</NavItem>
-                        <NavItem eventKey={1} href="#">Tops</NavItem>
+                        <NavItem eventKey={1} href="#">Inicio</NavItem>
+                        <NavDropdown title="Maquillaje" id="basic-nav-dropdown">
+                            <NavItem eventKey={2} href="#">opcion 1</NavItem>
+                            <NavItem eventKey={3} href="#">opcion 2</NavItem>
+                            <NavItem eventKey={4} href="#">opcion 3</NavItem>
+                        </NavDropdown>
+                        <NavDropdown title="Cursos">
+                            <NavItem eventKey={2} href="#">opcion 1</NavItem>
+                            <NavItem eventKey={3} href="#">opcion 2</NavItem>
+                            <NavItem eventKey={4} href="#">opcion 3</NavItem>
+                        </NavDropdown>
+                        <NavDropdown title="Cejas">
+                            <NavItem eventKey={2} href="#">opcion 1</NavItem>
+                            <NavItem eventKey={3} href="#">opcion 2</NavItem>
+                            <NavItem eventKey={4} href="#">opcion 3</NavItem>
+                        </NavDropdown>
+                        <NavItem eventKey={1} href="#">Contacto</NavItem>
+                        
                     </Nav>
 
                     <Nav pullRight>
-                        <NavItem bsStyle="primary">Busqueda</NavItem>
-                        <NavItem bsStyle="primary">Facebook</NavItem>                                        
+                        <NavItem bsStyle="primary"><input type='text'/><Button> Buscar </Button></NavItem>                                      
                         <NavItem bsStyle="primary">Ingresar</NavItem>                        
                         <NavItem bsStyle="success">Registrate</NavItem>                        
                     </Nav>

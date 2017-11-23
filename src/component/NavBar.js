436f1e96be9566f27da1acb09b575b444ce3c43f
Juan C. Rodriguez <jcrodriguezw@gmail.com>
2017-11-23 14:22:58 -0500

    agregando estilos con react-bootstrap al NavBar

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,28 +1,64 @@
 import React from 'react'
+import Button from 'react-bootstrap/lib/Button';
+import Navbar from 'react-bootstrap/lib/Navbar'
+import Nav from 'react-bootstrap/lib/Nav'
+import NavItem from 'react-bootstrap/lib/NavItem'
+import MenuItem from 'react-bootstrap/lib/MenuItem'
+import NavDropdown from 'react-bootstrap/lib/NavDropdown'
 
 class NavBar extends React.Component {
     render() {
         return(
-            <div>
-                <h1> ReCoTuTo </h1>
-                <div>
-                    <ul>
-                        <li> <a href="#"> Posts </a> </li>
-                        <li> <a href="#"> Shouts </a> </li>
-                        <li> <a href="#"> Comunidades </a> </li>
-                        <li> <a href="#"> Juegos </a> </li>
-                        <li> <a href="#"> Tops </a> </li>
-                    </ul>
-                </div>
-                <div>
-                    <button> search </button>
-                    <button> Facebook </button>
-                    <button> Ingresar </button>
-                    <button> Registrate </button>
-                </div>
-            </div>
+            <Navbar>
+
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#"> ReCoTuTo </a>
+                    </Navbar.Brand>
+                </Navbar.Header>
+
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem eventKey={1} href="#">Posts</NavItem>
+                        <NavItem eventKey={1} href="#">Shouts</NavItem>
+                        <NavItem eventKey={1} href="#">Comunidades</NavItem>
+                        <NavItem eventKey={1} href="#">Juegos</NavItem>
+                        <NavItem eventKey={1} href="#">Tops</NavItem>
+                    </Nav>
+
+                    <Nav pullRight>
+                        <NavItem bsStyle="primary">Busqueda</NavItem>
+                        <NavItem bsStyle="primary">Facebook</NavItem>                                        
+                        <NavItem bsStyle="primary">Ingresar</NavItem>                        
+                        <NavItem bsStyle="success">Registrate</NavItem>                        
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         )
     }
 }
 
 export default NavBar;
+
+
+{/* <Navbar.Header>
+<Navbar.Brand>
+    <a href="#"> ReCoTuTo </a>
+</Navbar.Brand>
+</Navbar.Header>
+
+<Nav.Collapse>
+<Nav>
+    <NavItem eventKey={1} href="#">Posts</NavItem>
+    <NavItem eventKey={1} href="#">Shouts</NavItem>
+    <NavItem eventKey={1} href="#">Comunidades</NavItem>
+    <NavItem eventKey={1} href="#">Juegos</NavItem>
+    <NavItem eventKey={1} href="#">Tops</NavItem>
+</Nav>
+<Nav>
+    <Button bsStyle="primary">Busqueda</Button>
+    <Button bsStyle="primary">Facebook</Button>
+    <Button bsStyle="primary">Ingresar</Button>
+    <Button bsStyle="success">Registrate</Button>
+</Nav>
+</Nav.Collapse>     */}

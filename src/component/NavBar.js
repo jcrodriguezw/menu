468fa3422d1014f4c7d14759cccfb8400fc38214
Juan C. Rodriguez <jcrodriguezw@gmail.com>
2017-11-23 13:14:16 -0500

    inicio del menu

---
+++ src/component/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class NavBar extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1> ReCoTuTo </h1>
+                <div>
+                    <ul>
+                        <li> <a href="#"> Posts </a> </li>
+                        <li> <a href="#"> Shouts </a> </li>
+                        <li> <a href="#"> Comunidades </a> </li>
+                        <li> <a href="#"> Juegos </a> </li>
+                        <li> <a href="#"> Tops </a> </li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NavBar;
